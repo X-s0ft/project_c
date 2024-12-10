15bc98c49b5706b53873f262d18627194762ef0e
Данила <d4vanchik@yandex.ru>
2024-12-10 19:49:15 +0300

    v0.0.1

---
+++ flutter_schedule/lib/routers/router.dart
@@ -0,0 +1,8 @@
+import '../pages/allpages.dart';
+
+final rout = {
+  '/Login': (context) => const Login(),
+  '/DateData': (context) => const DateData(),
+  '/Registr': (context) => const Registr(),
+  '/Options': (context) => const Options(),
+};
