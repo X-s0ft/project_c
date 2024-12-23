3476634ac46fd859e772bc4802f639cbe018448b
Данила <d4vanchik@yandex.ru>
2024-12-23 09:58:09 +0300

    v0.0.6

--- flutter_schedule/lib/pages/date_data.dart
+++ flutter_schedule/lib/pages/date_data.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class DateData extends StatefulWidget {
   const DateData({super.key});
@@ -10,17 +12,52 @@ class DateData extends StatefulWidget {
 class _DateDataState extends State<DateData> {
   @override
   Widget build(BuildContext context) {
+    String argument = ModalRoute.of(context)!.settings.arguments
+        as String; // Аргумент на профиль с прошлой страницы
 
+    void getdate_data() async {
+      try {
+        CollectionReference ref =
+            FirebaseFirestore.instance.collection(argument);
+
+        QuerySnapshot collref = await ref.get();
+
+        for (DocumentSnapshot doc in collref.docs) {
+          var datedoc = doc['Date'];
+          var scheduledoc = doc['Schedule'];
+
+          print('$datedoc, $scheduledoc');
+        }
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Ой, что-то пошло не так'),
+            duration: Duration(seconds: 3),
+            backgroundColor: Colors.red,
+          ),
+        );
+      }
+    }
 
     return Scaffold(
       appBar: AppBar(
-      
         title: const Text(
           'Дата',
-         
         ),
       ),
-      // body: ,
+      body: Align(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextButton(
+                onPressed: () {
+                  getdate_data();
+                },
+                child: Text('Нажми протестить'))
+          ],
+        ),
+      ),
     );
   }
 }
